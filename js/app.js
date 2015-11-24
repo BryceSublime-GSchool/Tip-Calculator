f4d9ae3f4efbf8d203d80ea6c8f59921cc29f7b9
Bryce Leonard <bryce.a.leonard@gmail.com>
2015-11-24 12:15:15 -0700

    updated JS

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ function calcTip(bill){
 
 var tip = document.getElementById("tip");
 var bill = document.getElementById("bill").value;
-tip.innerHTML = "$"+(bill * .2).toFixed(2);
+var percentage = document.getElementById("percentage").value;
+tip.innerHTML = "$"+ (bill * percentage).toFixed(2);
 
 }
